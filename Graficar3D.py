ab248272f40df8a333116916154632bbb136ccdd
xanderMMR <40546745+xanderMMR@users.noreply.github.com>
2019-10-31 14:26:30 -0500

    Update Graficar3D.py
    Grafica los bloques en 3D y con colores aleatorios

--- Graficar3D.py
+++ Graficar3D.py
@@ -1,36 +1,45 @@
+import matplotlib.pyplot as from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
 from itertools import product, combinations
+import random as rnd
+import time
 fig = plt.figure()
 ax = fig.gca(projection='3d')
+n=10
+arrCajas=[]
 
- 
-#draw cube
-r = [0,10]
-q=[10,20]
- 
-#for s, e in combinations(np.array(list(product(r,r,r))), 2):
- #   if np.sum(np.abs(s-e)) == 10:
-  #      ax.plot3D(*zip(s,e), color="b")
-#for s, e in combinations(np.array(list(product(q,[0,10],[0,10]))), 2):
- #   if np.sum(np.abs(s-e)) == 10:
- #       ax.plot3D(*zip(s,e), color="r")
-#for s, e in combinations(np.array(list(product(q,[0,10],[10,20]))), 2):
- #   if np.sum(np.abs(s-e)) == 10:
-  #      ax.plot3D(*zip(s,e), color="r")
-ap=[[0,20],[0,10],[0,20]]
-ab=[[7,10],[4,7],[4,17]]
-for s, e in combinations(np.array(list(product(ap[0],ap[1],ap[2]))), 2):
-    if np.sum(np.abs(s-e)) == ap[0][1]-ap[0][0] or np.sum(np.abs(s-e)) == ap[1][1]-ap[1][0] or np.sum(np.abs(s-e)) == ap[2][1]-ap[2][0] :
-        ax.plot3D(*zip(s,e), color="y")
-for s, e in combinations(np.array(list(product(ab[0],ab[1],ab[2]))), 2):
-    if np.sum(np.abs(s-e)) == ab[0][1]-ab[0][0] or np.sum(np.abs(s-e)) == ab[1][1]-ab[1][0] or np.sum(np.abs(s-e)) == ab[2][1]-ab[2][0] :
-        ax.plot3D(*zip(s,e), color="r")
-for s, e in combinations(np.array(list(product([1,3],[2,3],[4,6]))), 2):
-    if np.sum(np.abs(s-e)) == 3-1 or np.sum(np.abs(s-e)) == 1-ap[1][0] or np.sum(np.abs(s-e)) == 2 :
-        ax.plot3D(*zip(s,e), color="g")
-for s, e in combinations(np.array(list(product([15,17],[6,10],[10,20]))), 2):
-    if np.sum(np.abs(s-e)) == 2 or np.sum(np.abs(s-e)) == 4 or np.sum(np.abs(s-e)) == 10 :
-        ax.plot3D(*zip(s,e), color="b")
- 
+for i in range (n):
+    xc=i+10
+    posX=rnd.randint(i,10000)
+    posY=rnd.randint(i,10000)
+    posZ=rnd.randint(i,10000)
+    lenX=rnd.randint(1,30)
+    lenY=rnd.randint(1,30)
+    lenZ=rnd.randint(1,30)
+    x=[posX,lenX]
+    y=[posY,lenY]
+    z=[posZ,lenZ]
+    if posX>lenX:
+        x=[lenX,posX]
+    if posY>lenY:
+        y=[lenY,posY]
+    if posZ>lenZ:
+        z=[lenZ,posZ]
+    arrCajas.append([x,y,z])
+
+
+rgb=[None]*n
+print(rgb)
+
+for i in range(n):
+    r=rnd.randint(0,1)
+    g=rnd.randint(0,1)
+    b=rnd.randint(0,1)
+    rgb[i]=[r,g,b]
+        
+for k in range (n):
+    for s, e in combinations(np.array(list(product(arrCajas[k][0],arrCajas[k][1],arrCajas[k][2]))), 2):
+        if np.sum(np.abs(s-e)) == arrCajas[k][0][1]-arrCajas[k][0][0] or np.sum(np.abs(s-e)) == arrCajas[k][1][1]-arrCajas[k][1][0] or np.sum(np.abs(s-e)) == arrCajas[k][2][1]-arrCajas[k][2][0] :
+            ax.plot3D(*zip(s,e), color=rgb[k])
 plt.show()
